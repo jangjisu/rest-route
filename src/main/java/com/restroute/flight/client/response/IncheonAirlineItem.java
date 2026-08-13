d598322a2190fc671e6434880c43998939ae0f87
jangjisu <58325722+jangjisu@users.noreply.github.com>
2026-08-13 14:12:19 +0900

    refactor: 라이브 API 방식이던 인천/Travelpayouts JSON 클라이언트 삭제
    참조데이터(국가/도시/공항/항공사)가 SQL 시딩 구조로 전환되면서 더 이상
    런타임에 인천국제공항공사 API나 Travelpayouts cities/countries/airlines.json을
    직접 호출하지 않는다. IncheonClient/IncheonFeignClient와 그 응답 DTO,
    Travelpayouts의 citiesData/countriesData/airlinesData 메서드 및 대응 DTO,
    그리고 이를 소비하던 FlightCityEntity.from/FlightCountryEntity.from
    팩토리 메서드(호출부 없음)를 삭제했다. application.properties의
    incheon.api.* 프로퍼티도 함께 정리.

--- src/main/java/com/restroute/flight/client/response/IncheonAirlineItem.java
+++
@@ -1,9 +0,0 @@
-package com.restroute.flight.client.response;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record IncheonAirlineItem(
-        @JsonProperty("airlineIata") String iataCode,
-        @JsonProperty("airlineName") String name) {}
